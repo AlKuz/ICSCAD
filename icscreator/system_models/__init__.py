c6f6bfc2c59439bd26ddeb3fa9cba6ccf77c2fe1
Alexander Kuznetsov <a.v.kuznetsov31@gmail.com>
2018-11-28 22:18:43 +0400

    Add Model and Element classes

--- icscreator/system_models/__init__.py
+++ icscreator/system_models/__init__.py
@@ -1,5 +1,7 @@
 """
 System models.
 """
+
 from icscreator.system_models.element import Element
 from icscreator.system_models.system import System
+from icscreator.system_models.model import Model
