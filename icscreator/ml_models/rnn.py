021edd69cd20f650ba9b8e28c98659c3dd141ae5
Alexander Kuznetsov <a.v.kuznetsov31@gmail.com>
2019-07-12 11:51:33 +0400

    Add base network class

--- icscreator/ml_models/rnn.py
+++ icscreator/ml_models/rnn.py
@@ -6,13 +6,29 @@ ElmanNetwork
 import tensorflow as tf
 
 
-class ElmanNetwork(object):
+class NeuralNetwork(object):
+    """Base class of neural networks"""
+
+    def __init__(self, seed=None):
+        self._seed = seed
+
+    def _layer(self, tensor: tf.Tensor, num_outputs, activation=tf.sigmoid, name='layer'):
+        num_inputs = int(tensor.shape[1])
+        with tf.name_scope(name):
+            biases = tf.Variable(tf.random.uniform([num_outputs], -1, 1, seed=self._seed, name='biases'))
+            weights = tf.Variable(tf.random.uniform([num_inputs, num_outputs], -1, 1, seed=self._seed, name='weights'))
+            result = activation(tf.matmul(tensor, weights) + biases)
+        return result
+
+
+class ElmanNetwork(NeuralNetwork):
+    """Elman network"""
 
     def __init__(self, inputs: int, hiddens: int, outputs: int, seed=None):
+        super().__init__(seed)
         self._inputs = inputs
         self._hiddens = hiddens
         self._outputs = outputs
-        self._seed = seed
         self._graph = tf.Graph()
         self._model_inputs = None
         self._model_outputs = None
@@ -23,16 +39,9 @@ class ElmanNetwork(object):
             self._model_inputs = tf.compat.v1.placeholder(tf.float32, [1, self._inputs], 'inputs')
             state = tf.Variable(tf.zeros([1, self._hiddens]), trainable=False, name='state')
 
-            with tf.name_scope('hidden'):
-                wh = tf.Variable(tf.random.uniform([self._inputs, self._hiddens], -1, 1, seed=self._seed, name='wh'))
-                ws = tf.Variable(tf.random.uniform([self._hiddens, self._hiddens], -1, 1, seed=self._seed, name='ws'))
-                bh = tf.Variable(tf.random.uniform([self._hiddens], -1, 1, seed=self._seed, name='bh'))
-                hiddens = tf.assign(state, tf.sigmoid(tf.matmul(self._model_inputs, wh) + tf.matmul(state, ws) + bh))
-
-            with tf.name_scope('output'):
-                wo = tf.Variable(tf.random.uniform([self._hiddens, self._outputs], -1, 1, seed=self._seed, name='wo'))
-                bo = tf.Variable(tf.random.uniform([self._outputs], -1, 1, seed=self._seed, name='bo'))
-                self._model_outputs = tf.sigmoid(tf.matmul(hiddens, wo) + bo, name='outputs')
+            hiddens = self._layer(tf.concat([self._model_inputs, state], axis=1), self._hiddens, name='hidden')
+            hiddens = tf.assign(state, hiddens)
+            self._model_outputs = self._layer(hiddens, self._outputs, name='output')
 
             writer = tf.summary.FileWriter('tb', tf.get_default_graph())
 
