6e3b80a8c6f4d49afbfe54898b4a12a28dfa95de
Alexander Kuznetsov <a.v.kuznetsov31@gmail.com>
2019-07-20 19:39:33 +0400

    Change NeuralNetwork class for working with Layer class

--- icscreator/ml_models/rnn.py
+++ icscreator/ml_models/rnn.py
@@ -3,6 +3,8 @@ Recurrent neural networks:
 
 ElmanNetwork
 """
+import numpy as np
+import os
 import tensorflow as tf
 from abc import abstractmethod
 
@@ -120,116 +122,96 @@ class Layer(object):
 class NeuralNetwork(object):
     """Base class of neural networks"""
 
-    def __init__(self, seed=None):
+    def __init__(self,
+                 loss=tf.losses.mean_squared_error,
+                 optimizer=tf.train.AdamOptimizer,
+                 optimizer_settings={},
+                 name='neural_network',
+                 model_folder=os.getcwd()):
         """
-        Initialization of base network class
-
-        Args:
-            seed (int): Seed for the random generator to create network weights
+        Initialization of base network class.
         """
-        self._seed = seed
+        self._name = name
+        self._model_folder = model_folder
+        self._session = tf.Session()
+        self._model_inputs = None
+        self._model_outputs = None
+        self._model_targets = None
+        self._model_loss = None
+        self._model_optimizer = None
 
-    def _layer(self, tensor: tf.Tensor, num_outputs: int, activation=tf.sigmoid, name='layer'):
-        """
-        Full connected layer
+        self.compile(loss, optimizer, **optimizer_settings)
 
-        Args:
-            tensor (tf.Tensor): Input tensor in the layer
-            num_outputs (int): Number of output neurons in the layer
-            activation (func): Activation function of the layer. Initially it is a sigmoid
-            name (str): Name of the layer
+    def compile(self,
+                loss=tf.losses.mean_squared_error,
+                optimizer=tf.train.AdamOptimizer,
+                **optimizer_settings) -> None:
 
-        Return (tf.Tensor): Output tensor of the layer
-        """
-        assert len(tensor.shape) == 1
-        num_inputs = int(tensor.shape[0])
+        self._session.as_default()
+        with tf.name_scope(self._name):
+            self._create_model()
 
-        with tf.name_scope(name):
-            biases = tf.Variable(tf.random.uniform([num_outputs], -1, 1, seed=self._seed, name='biases'))
-            weights = tf.Variable(tf.random.uniform([num_outputs, num_inputs], -1, 1, seed=self._seed, name='weights'))
-            result = activation(tf.linalg.matvec(weights, tensor) + biases)
+        assert isinstance(self._model_inputs, tf.Tensor)
+        assert isinstance(self._model_outputs, tf.Tensor)
+        self._model_targets = Layer.input(self._model_outputs.shape[1], name='targets')
 
-        return result
-
-    def _lstm(self, tensor, num_outputs: int, name='lstm'):
-        with tf.name_scope(name):
-            state = tf.Variable(tf.zeros([num_outputs]), trainable=False, name='state')
-            hidden = tf.Variable(tf.zeros([num_outputs]), trainable=False, name='hidden')
-
-            inputs = tf.concat([hidden, tensor], axis=0)
-
-            forget_gate = self._layer(inputs, num_outputs, name='forget_gate')
-            input_gate = self._layer(inputs, num_outputs, name='input_gate')
-            candidate_gate = self._layer(inputs, num_outputs, activation=tf.tanh, name='candidate_gate')
-
-            state = tf.assign(state, forget_gate * state + input_gate * candidate_gate)
-
-            output_gate = self._layer(inputs, num_outputs, name='output_gate')
-
-            hidden = tf.assign(hidden, output_gate * tf.tanh(state))
-
-            return hidden
-
-    def _delayed(self, tensor: tf.Tensor, num_delays: int, name='delays'):
-        assert len(tensor.shape) == 1
-        num_inputs = int(tensor.shape[0])
-
-        with tf.name_scope(name):
-            delays = tf.Variable(tf.zeros([num_inputs * (num_delays + 1)]), trainable=False, name='tensor_delays')
-            concatenated = tf.concat([tensor, delays[:-num_inputs]], axis=0)
-            delays = tf.assign(delays, concatenated)
-        return delays
+        self._model_loss = loss(self._model_targets, self._model_outputs)
+        self._model_optimizer = optimizer(**optimizer_settings).minimize(self._model_loss)
+        self._session.run(tf.initialize_all_variables())
 
     @abstractmethod
     def _create_model(self):
+        """
+        Method for model creating.
+
+        Use:
+            self._model_inputs for input placeholder
+            self._model_outputs for output tensor
+        """
         pass
 
-
-class ElmanNetwork(NeuralNetwork):
-    """Elman network"""
-
-    def __init__(self, inputs: int, hiddens: int, outputs: int, seed=None):
-        super().__init__(seed)
-        self._inputs = inputs
-        self._hiddens = hiddens
-        self._outputs = outputs
-        self._graph = tf.Graph()
-        self._model_inputs = None
-        self._model_outputs = None
-        self._create_model()
-
-    def _create_model(self):
-        with self._graph.as_default():
-            self._model_inputs = tf.compat.v1.placeholder(tf.float32, [self._inputs], 'inputs')
-            state = tf.Variable(tf.zeros([self._hiddens]), trainable=False, name='state')
-
-            hiddens = self._layer(tf.concat([self._model_inputs, state], axis=0), self._hiddens, name='hidden')
-            hiddens = tf.assign(state, hiddens)
-            self._model_outputs = self._layer(hiddens, self._outputs, name='output')
-            self._model_outputs = self._delayed(self._model_outputs, 5)
-
-            writer = tf.summary.FileWriter('tb', tf.get_default_graph())
-
     def predict(self, data):
+        """
+        Make prediction of neural network.
+
+        Args:
+            data (list): Make predictions for list of values.
+
+        Returns (list): List of prediction results.
+        """
         result = []
-        with tf.Session(graph=self._graph) as sess:
-            sess.run(tf.initialize_all_variables())
-            for i in data:
-                r = sess.run(self._model_outputs, feed_dict={self._model_inputs: i})
-                result.append(list(r))
+        for i in data:
+            r = self._session.run(self._model_outputs, feed_dict={self._model_inputs: i})
+            r = list(r) if len(r) > 1 else float(r)
+            result.append(r)
         return result
 
-    @property
-    def hiddens(self):
-        return self._hiddens
+    def evaluate(self, input_data: list, target_data: list) -> float:
+        """
+        Evaluate neural network.
 
-    @property
-    def inputs(self):
-        return self._inputs
+        Args:
+            input_data (list): List of data to feed neural network.
+            target_data (list): List of data to compare network results.
 
-    @property
-    def outputs(self):
-        return self._outputs
+        Returns (float):
+        """
+        loss_result = []
+        for i, t in zip(input_data, target_data):
+            r = self._session.run(self._model_loss, feed_dict={self._model_inputs: i,
+                                                               self._model_targets: t})
+            loss_result.append(r)
+        loss_result = float(np.mean(loss_result))
+        return loss_result
+
+    def optimize(self, error: float):
+        pass
+
+    def train(self, input_data: list, target_data: list):
+        pass
+
+    def save(self, path: str, only_model=False):
+        pass
 
 
 if __name__ == "__main__":
